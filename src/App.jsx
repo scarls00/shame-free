ff8435db5c66e848b3d2adf6700671167224a042
scarls00 <seritarak@gmail.com>
2026-04-30 11:04:30 -0500

    update styling and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css'
 import Todo from './components/Todo.jsx'
 import Modal from './components/modal.jsx'  
+import { useState } from 'react'
+
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
       <h1>My To-do List</h1>
       <div className="todo__wrapper ">
         <Todo />
         <Todo />
-        <Todo />  
+        <Todo />
       </div>
-      <Modal />
+      <Modal onClose={() => setShowModal(false)} />
     </>
   )
 }
 
-const [showModal, setShowModal] = useState(false);
-<Modal onClose={() => setShowModal(false)} />
 
 export default App
